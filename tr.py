bf282e038099c60ab2f096531eecf9ccfbf28f50
dacomo1920daw2 <dacomo1920daw2@gmail.com>
2020-02-07 13:12:47 +0100

    Commit 2 branca tr02

--- tr.py
+++ tr.py
@@ -11,10 +11,10 @@ import math # Mòdul math amb les funcions math.sqrt() i atan
 def tr(a,b):
 	hip=math.sqrt((a**2)+(b**2))
 	ang=math.atan(a/b)*(180.0/math.pi)
-	print "La hipotenusa és: %.2f" % (hip)
-	print "L'angle és: %.2f graus" % (ang)
+	print "La hipotenusa és: %.3f" % (hip)
+	print "L'angle és: %.3f graus" % (ang)
 	area=(a*b)/2
-	print "L'àrea és: %.2f" % (area)
+	print "L'àrea és: %.3f" % (area)
 	return
 #
 # PROGRAMA PRINCIPAL
