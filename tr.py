f37c8e816299182d66e07530ce1f73696701be38
dacomo1920daw2 <dacomo1920daw2@gmail.com>
2020-02-07 12:54:18 +0100

    Commit 1 branca master

---
+++ tr.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+"""Nom del codi: tr.py"""
+# Treballant amb funcions - I
+#
+# IMPORTS
+import math # Mòdul math amb les funcions math.sqrt() i atan 
+#
+# FUNCIONS
+#
+def tr(a,b):
+	hip=math.sqrt((a**2)+(b**2))
+	ang=math.atan(a/b)*(180.0/math.pi)
+	print "La hipotenusa és: %.2f" % (hip)
+	print "L'angle és: %.2f graus" % (ang)
+	return
+#
+# PROGRAMA PRINCIPAL
+a = input("Catet oposat a: ")
+b = input("Costat adjacent b: ")
+tr(a,b)
